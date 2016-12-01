7cd6f404c19e4db5424bdd1c698e59dc42d9df6e
yuri-swift <yuri.swift.615@gmail.com>
2016-12-01 23:10:04 +0900

    firstCommit

---
+++ cutomePopup/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  cutomePopup
+//
+//  Created by 田山　由理 on 2016/12/01.
+//  Copyright © 2016年 Yuri Tayama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
